8dd9be4d27b14d43cffa50f0b22bb9cbe3afa166
MaxHaGever <maxspectorr@gmail.com>
2026-01-29 11:02:48 +0200

    Fixed app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,18 @@ import whatsappRoutes from "./routes/whatsappWebhookRoute";
 
 const app = express();
 
+app.use((req, _res, next) => {
+  console.log("INCOMING:", req.method, req.originalUrl, "content-type:", req.headers["content-type"]);
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/whatsapp", whatsappRoutes);
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ ok: true });
 });
-
-app.use
 
 export default app;
